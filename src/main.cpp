bd81cfd7ff3cfe6179181f0b45109d90e4067fc9
Simone Gaiarin <simgunz@gmail.com>
2024-06-29 15:58:34 +0200

    Make minimal example failure case for cppcoreguidelines-init-variables

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,5 @@
-#include <App.h>
 #include <Arduino.h>
-#include <display/Display.h>
 
-const int CLOCK_TICK_DURATION_MS = 1000;
+void setup() { String buffer{"Hello"}; }
 
-App *app;
-
-// cppcheck-suppress unusedFunction
-void setup()
-{
-    Serial.begin(115200);
-    Display *display = Display::create_display(DISPLAY_TYPE);
-    app = new App(display);
-}
-
-// cppcheck-suppress unusedFunction
-void loop()
-{
-    app->update_text();
-    delay(CLOCK_TICK_DURATION_MS);
-}
+void loop() { }
